eb73de8d55683c34992fc698049f9e09bc4a5439
cryptongithub <96590783+cryptongithub@users.noreply.github.com>
2022-03-30 18:50:31 +0200

    Update and rename penumbra_laucnh.sh to penumbra_wallet.sh

--- penumbra_wallet.sh
+++ penumbra_wallet.sh
@@ -1,4 +1,5 @@
-#!/bin/baif curl > /dev/null 2>&1; then
+#!/bin/baif 
+if curl > /dev/null 2>&1; then
 	echo ''
 else
   sudo apt install curl -y
@@ -8,14 +9,22 @@ clear && curl -s https://raw.githubusercontent.com/cryptongithub/init/main/logo.
 
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install make tar wget clang pkg-config libssl-dev jq build-essential -y
+sudo apt install make git tar wget clang pkg-config libssl-dev jq build-essential -y
 
-if git > /dev/null 2>&1; then
-	echo ''
-else
-  sudo apt install git -y
-fi
+echo -e '\n\e[42m==================================================\e[0m\n' 
+echo -e '\e[32mPress \e[31m1 and Enter\e[0m \e[32mwhen you will be promted.\e[0m'
+echo -e '\n\e[42m==================================================\e[0m\n' && sleep 5
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.cargo/env 
+
+rm -rf $HOME/penumbra && git clone https://github.com/penumbra-zone/penumbra && cd $HOME/penumbra
+
+git checkout 005-mneme
+
+cargo build --release --bin pcli 
+
+cargo run --quiet --release --bin pcli wallet generate
 
 ADDRESS=$(cargo run --quiet --release --bin pcli addr list | grep -oP '(?<=penumbra).*')
 
@@ -23,7 +32,7 @@ echo -e '\n\e[42m==================================================\e[0m\n'
 echo -e '\e[42mSAVE ALL DATA BELOW\e[0m'
 echo -e '\n\e[42m==================================================\e[0m\n' && sleep 2
 
-echo -e 'Address:'
+echo -e '\nAddress:'
 echo 'penumbra'$ADDRESS
 echo -e '\nWallet info and spend seed:'
 cat /root/.local/share/pcli/penumbra_wallet.json && echo -e '\n'
